744b48c16fdba6adcca317bcb26b6730f3c9cd23
Skifok <taswdt@gmail.com>
2016-07-02 02:12:59 +0300

    Try

--- ProtoPtr/ProtoPtr/ProtoPtr.h
+++ ProtoPtr/ProtoPtr/ProtoPtr.h
@@ -4,13 +4,37 @@
 class proto_ptr {
 public:
 	proto_ptr():ptr(nullptr) {}
-	proto_ptr(int * ptr):ptr(ptr) {}
-	proto_ptr(const proto_ptr& Right) = delete;
+	proto_ptr(test * ptr):ptr(ptr) {}
 	proto_ptr& operator=(const proto_ptr& Right) = delete;
-	int operator *() {return *ptr;}
-	int get() { return *ptr; }
-
+	test operator *() {return *ptr;}
+	test* get() { return ptr; }
+	test* realese() {
+		test * temp = ptr;
+		reset();
+		return temp;
+	}
+	void reset() {
+		 ptr= nullptr;
+	}
+	void swap(proto_ptr& foo) {
+		test* temp = ptr;
+		ptr = foo.ptr;
+		foo.ptr = temp;
+	}
+	~proto_ptr() {
+		delete ptr;
+	}
 
 private:
-	int * ptr;
+	test * ptr;
+};
+class test {
+	double* mass;
+public : 
+	test():mass(new double[30]) {
+		
+	}
+	~test() {
+		delete[] mass;
+	}
 };
--- ProtoPtr/ProtoPtr/test.cpp
+++ ProtoPtr/ProtoPtr/test.cpp
@@ -1,16 +1,11 @@
 #include "ProtoPtr.h"
 #include <memory>
 int main() {
-	int b = 1024;
-	int bc = 332;
-	int * k = &b;
-	std::unique_ptr<int> a(&b);// = std::make_unique<int*>(k);
-	std::unique_ptr<int> boo(&bc);
-	k = a.release();
-	std::cout<<a.get()<<std::endl;
-	a.swap(boo);
-	std::cout << a.get() << std::endl;
-
+	test a, b;
+	
+	std::unique_ptr<test> k(&a);// = std::make_unique<int*>(k);
+	proto_ptr clon(&b);
+	
 	std::cin.get();
 
 	return 0;
