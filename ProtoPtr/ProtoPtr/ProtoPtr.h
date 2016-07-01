57e85753b2a3cfb6462de1dea2139539dafea83e
Skifok <taswdt@gmail.com>
2016-07-02 02:16:47 +0300

    I love linker

--- ProtoPtr/ProtoPtr/ProtoPtr.h
+++ ProtoPtr/ProtoPtr/ProtoPtr.h
@@ -1,12 +1,25 @@
 #pragma once
 #include <iostream>
 //template <class T>
+class test {
+	double* mass;
+public:
+	test() :mass(new double[30]) {
+
+	}
+	~test() {
+		delete[] mass;
+	}
+};
 class proto_ptr {
 public:
 	proto_ptr():ptr(nullptr) {}
 	proto_ptr(test * ptr):ptr(ptr) {}
 	proto_ptr& operator=(const proto_ptr& Right) = delete;
-	test operator *() {return *ptr;}
+	test operator *()
+	{
+		return *ptr;
+	}
 	test* get() { return ptr; }
 	test* realese() {
 		test * temp = ptr;
@@ -27,14 +40,4 @@ public:
 
 private:
 	test * ptr;
-};
-class test {
-	double* mass;
-public : 
-	test():mass(new double[30]) {
-		
-	}
-	~test() {
-		delete[] mass;
-	}
 };
